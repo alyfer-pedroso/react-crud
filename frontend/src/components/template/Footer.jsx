8e79f0d4647cd4848e4d45f6e1bdc95c83797751
Alyfer Pedroso <138719366+alyfer-pedroso@users.noreply.github.com>
2023-12-08 00:17:35 -0300

    commit

---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./Footer.css";
+
+export default (props) => (
+    <footer className="footer">
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por{" "}
+            <strong>
+                Alyfer <span className="text-danger">Pedroso</span>
+            </strong>
+        </span>
+    </footer>
+);
